5030ea56f045db3f350506b68be07314d06496ce
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-02-01 14:16:01 -0700

    commenting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -10,7 +11,18 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
+});
+//  response.render('pages/index');
+//});
+
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
