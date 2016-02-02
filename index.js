3801f961fbb7361e8f09517ece4a3c744b882840
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-02-02 10:40:21 -0700

    adding session storage in Mongo

--- index.js
+++ index.js
@@ -2,37 +2,38 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo/es5')(session);
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//The root URL
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
+  var result = '';
+  var times = process.env.TIMES || 5;
   for (i=0; i < times; i++)
     result += cool();
   response.send(result);
 });
-var mongodbUri = process.env.MONGOLAB_URI;
 
+// Setting up the database connection and session storage
+var mongodbUri = process.env.MONGOLAB_URI || 'localhost:27017/nodetest1';
 mongoose.connect(mongodbUri);
-
 var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', console.error.bind(console, 'connection success!'));
-
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+app.use(session({
+    store: new MongoStore({ mongooseConnection: mongoose.connection})
+}));
+db.on('error', console.error.bind(console, 'Database connection error:'));
+db.once('open', console.error.bind(console, 'Connected to MongDB'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
