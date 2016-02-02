d29ba85477a640f90dcd304935bc476d57c3a590
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-02-02 12:33:03 -0700

    checking in login code

--- index.js
+++ index.js
@@ -1,42 +1,72 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var mongodb = require('mongodb');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
+// not sure about this one
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 //The root URL
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   var result = '';
   var times = process.env.TIMES || 5;
   for (i=0; i < times; i++)
     result += cool();
   response.send(result);
-});
+}); */
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Setting up the database connection and session storage
 var mongodbUri = process.env.MONGOLAB_URI || 'localhost:27017/nodetest1';
 mongoose.connect(mongodbUri);
 var db = mongoose.connection;
 app.use(session({
-    store: new MongoStore({ mongooseConnection: mongoose.connection})
+    secret: 'foo', store: new MongoStore({ mongooseConnection: mongoose.connection})
 }));
 db.on('error', console.error.bind(console, 'Database connection error:'));
 db.once('open', console.error.bind(console, 'Connected to MongDB'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+module.exports = app;
