89449ee179b75ccadeed69dea6feb67920eab246
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-07-05 18:46:07 -0600

    changing layout of the idea profile page and deleting some unused files

--- routes/idea-data-routes.js
+++ routes/idea-data-routes.js
@@ -20,90 +20,6 @@ var uploading = multer({
 
 
 ////////////////////////////////////////////////
-// Alternative Ideas
-////////////////////////////////////////////////
-router.get('/alternative-ideas', function(req, res) {
-  if(!req.session.idea){
-    res.redirect('/');
-    return;
-  }
-  IdeaSeed.findById(req.session.idea,function(err, idea){
-    currentIdea = idea._doc;
-    res.render('pages/idea-data/idea-alternatives', {
-      user : req.user, idea : currentIdea });
-  });
-});
-
-router.post('/alternative-ideas', function(req, res) {
-  var alternativeIdea = {
-    name          : req.body.alternativeName,
-    failureReason : req.body.failureReason,
-    description   : req.body.description,
-    differsBy     : req.body.differsBy
-
-  };
-
-  IdeaSeed.update(
-    { _id : req.session.idea },
-    { $push : { alternatives : alternativeIdea }},
-    function(err, raw){
-      console.log('The raw response from Mongo was ', raw);
-    }
-  );
-
-  Account.findById( req.user.id,
-    function (err, account) {
-        account.einsteinPoints = account.einsteinPoints + 5;
-        account.save(function (err) {
-      });
-  });
-  res.redirect('/alternative-ideas');
-
-});
-
-////////////////////////////////////////////////
-// Industries
-////////////////////////////////////////////////
-router.get('/industries', function(req, res) {
-  if(!req.session.idea){
-    res.redirect('/');
-    return;
-  }
-  IdeaSeed.findById(req.session.idea,function(err, idea){
-    currentIdea = idea._doc;
-    res.render('pages/idea-data/industries', {
-      user : req.user, idea : currentIdea });
-  });
-});
-
-router.post('/alternative-ideas', function(req, res) {
-  var alternativeIdea = {
-    name          : req.body.alternativeName,
-    failureReason : req.body.failureReason,
-    description   : req.body.description,
-    differsBy     : req.body.differsBy
-
-  };
-
-  IdeaSeed.update(
-    { _id : req.session.idea },
-    { $push : { alternatives : alternativeIdea }},
-    function(err, raw){
-      console.log('The raw response from Mongo was ', raw);
-    }
-  );
-
-  Account.findById( req.user.id,
-    function (err, account) {
-        account.einsteinPoints = account.einsteinPoints + 5;
-        account.save(function (err) {
-      });
-  });
-  res.redirect('/alternative-ideas');
-
-});
-
-////////////////////////////////////////////////
 // Add a problem to an idea seed
 ////////////////////////////////////////////////
 router.post('/add-idea-problem', function(req, res) {
@@ -226,12 +142,7 @@ router.post('/add-profile-headshot',  function(req, res) {
       });
 
     }); //end of idea image query
-
-
-    
-
   }
-
 });
 
 ////////////////////////////////////////////////
@@ -256,10 +167,6 @@ router.post('/set-existing-profile-pic', function(req, res) {
         }
       });
     });
-
-
-
-
   }
 });
 
