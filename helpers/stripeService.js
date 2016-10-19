6ebd766d15ee7ae1b990acc730e106d7828fe694
billingb <billingb@gmail.com>
2016-10-19 13:14:52 -0600

    Fixed workflow problems with detecting if accounts are linked already or not. Carry idea name through campaign workflow so it can be referenced later in the flow. Refactor of campaign pages for consistency, clean things up.

--- helpers/stripeService.js
+++ helpers/stripeService.js
@@ -2,6 +2,7 @@ var exports = module.exports = {};
 var requestpromise = require('request-promise');
 var Account = require('../models/account');
 var StripeCredentials = require('../models/stripeCredentials');
+var Promise = require('bluebird');
 
 const TOKEN_URI = 'https://connect.stripe.com/oauth/token';
 
@@ -29,7 +30,8 @@ exports.charge = function(tokenId, amount) {
 };
 
 exports.connect = function(stripeInfo, user) {
-  return requestpromise({
+  var query = Account.findById( user.id);
+  var authPost = requestpromise({
     method: 'POST',
     url: TOKEN_URI,
     form: {
@@ -38,23 +40,26 @@ exports.connect = function(stripeInfo, user) {
       code: stripeInfo.code,
       client_secret: process.env.STRIPE_SECRET
     }
-  }).then(function(r) {
-    Account.findById( user.id, function (err, account) {
-      if(err) {
-        throw err;
-      }
-      if(!account){
-        throw new Error("Can't find user with username: " + account.username);
-      }
+  });
 
-      var stripeParsed = JSON.parse(r);
-      account.stripeCredentials = new StripeCredentials(stripeParsed);
-      account.save(function (err) {
-        if(err) {
-          throw err;
-        }
-        return account;
-      });
+  return Promise.all([query, authPost]).then(function(values) {
+    var account = values[0];
+    var postResponse = values[1];
+
+    if(!account){
+      throw new Error("Can't find user with username: " + account.username);
+    }
+
+    var stripeParsed = JSON.parse(postResponse);
+    var stripeCreds = new StripeCredentials(stripeParsed);
+    stripeCreds.save();
+    account.stripeCredentials = stripeCreds._id;
+    return account.save().then(function() {
+      if(stripeInfo.state) {
+        var stateParts = stripeInfo.state.split(',');
+        return stateParts[0];
+      }
+      return null;
     });
-  })
+  });
 };
--- routes/crowdfunding-routes.js
+++ routes/crowdfunding-routes.js
@@ -3,95 +3,106 @@ var router = express.Router();
 var csrf = require('csurf');
 var csrfProtection = csrf({ cookie: true });
 var stripeService = require('../helpers/stripeService');
+var Account = require('../models/account');
+var IdeaSeed = require('../models/ideaSeed');
 
-router.get('/new-campaign', csrfProtection, function(req, res){
+router.get('/ideas/:ideaName/campaign/new', csrfProtection, function(req, res){
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
-
-  res.render('pages/campaign/new-campaign', {
-    csrfToken: req.csrfToken(),
-    user: req.user
+  var query = IdeaSeed.findOne({"name" : req.params.ideaName});
+  query.exec().then(function(idea){
+    res.render('pages/campaign/new', {
+      csrfToken: req.csrfToken(),
+      user: req.user,
+      idea: idea
+    });
   });
 });
 
-router.post('/new-campaign', csrfProtection, function(req, res){
+router.post('/ideas/:ideaName/campaign/new', csrfProtection, function(req, res){
   //TODO: Hey - we need to actually save stuff here
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
 
-  if(req.user.stripeCredentials && req.user.stripeCredentials.access_token) {
-    res.render('pages/campaign/connect-campaign', {
-      csrfToken: req.csrfToken(),
-      user: req.user
-    });
-  } else {
-    res.render('pages/campaign/campaign', {
-      csrfToken: req.csrfToken(),
-      user: req.user
-    });
-  }
+  Account.findOne({'username': req.user.username}).then(function (account) {
+    if(account.stripeCredentials == undefined || account.stripeCredentials.access_token == undefined) {
+      res.redirect('/ideas/' + req.params.ideaName +'/campaign/connect');
+    } else {
+      res.redirect('/ideas/' + req.params.ideaName + '/campaign');
+    }
+  });
 });
 
-router.get('/connect-campaign', csrfProtection, function(req, res) {
+router.get('/ideas/:ideaName/campaign/connect', csrfProtection, function(req, res) {
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
 
-  res.render('pages/campaign/connect-campaign', {
-    csrfToken: req.csrfToken(),
-    user: req.user
-  })
+  var query = IdeaSeed.findOne({"name" : req.params.ideaName});
+  query.exec().then(function(idea){
+    res.render('pages/campaign/connect', {
+      csrfToken: req.csrfToken(),
+      user: req.user,
+      idea: idea
+    })
+  });
 });
 
 router.get('/stripe-campaign-connect', csrfProtection, function(req, res) {
-  //TODO: Need to handle multiple campaigns with /connect-campaign/<id> route
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
 
-  stripeService.connect(req.query, req.user)
-    .then(function() {
-      res.render('pages/campaign/campaign', {
-        csrfToken: req.csrfToken(),
-        user: req.user
-      });
+  var connectPromise = stripeService.connect(req.query, req.user);
+  connectPromise.then(function(ideaName) {
+      if(ideaName !== undefined && ideaName !== null) {
+        res.redirect('/ideas/' + ideaName + '/campaign');
+      } else {
+        res.redirect('/imagineer/' + req.user.nickname);
+      }
+      return;
     })
     .catch(function (err) {
       res.render('pages/404'); //TODO: make error page for connecting
     });
 });
 
-router.get('/campaign', csrfProtection, function(req, res){
-  //TODO: Need to handle multiple campaigns with /campaign/<id> route
+router.get('/ideas/:ideaName/campaign', csrfProtection, function(req, res){
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
 
-  res.render('pages/campaign/campaign', {
-    csrfToken: req.csrfToken(),
-    user: req.user
+  var query = IdeaSeed.findOne({"name" : req.params.ideaName});
+  query.exec().then(function(idea){
+    res.render('pages/campaign/view', {
+      csrfToken: req.csrfToken(),
+      user: req.user,
+      idea: idea
+    });
   });
 });
 
-router.post('/stripe-payment', csrfProtection, function(req, res){
+router.post('/ideas/:ideaName/campaign/stripe-payment', csrfProtection, function(req, res){
   if( !(req.user && req.user.username)){
     res.redirect('/');
     return;
   }
 
-  stripeService.charge(req.body.tokenId, req.body.amount).then(function (success){
-   if(success) {
+  stripeService
+    .charge(req.body.tokenId, req.body.amount, req.params.ideaName)
+    .then(function (success){
+    if(success) {
      res.send('success');
-   } else {
+    } else {
      res.status(400).send('Transaction failed');
-   }
+    }
   });
 });
 
